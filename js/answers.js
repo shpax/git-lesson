f8964ea7f60bd1205036f17e26fb783e6ac640e8
Maxim Piven <shpax1@gmail.com>
2019-06-05 16:48:11 +0300

    added runQuiz

--- js/answers.js
+++ js/answers.js
@@ -1,6 +1,21 @@
 function saveAnswer(question, text) {
+  let isCorrect = null;
+  
+  if (question.correctAnswer) isCorrect = question.correctAnswer === text;
+
   return {
     question,
     text,
+    isCorrect,
   }
+}
+
+
+function askQuestion(question) {
+  let questionText = question.text;
+  if (question.options) {
+    questionText += '\n' + question.options.map(o => `- ${o}`).join('\n')
+  }
+  
+  return prompt(questionText);
 }
--- js/quiz.js
+++ js/quiz.js
@@ -3,5 +3,24 @@
 function createQuiz(questions) {
   return {
     questions,
+    answers: null,
   }
 }
+
+const questionsList = [
+  createQuestion('name', 'Your name?'),
+  createQuestion('company', 'Your company?'),
+  createQuestion('developer', 'Wanna paly tennis?', ['Yes', 'No'], 'No'),
+]
+
+function runQuiz(quiz) {
+  quiz.answers = quiz.questions.map(question => {
+    const answerText = askQuestion(question);
+
+    return saveAnswer(question, answerText);
+  })
+
+  return quiz;
+}
+
+console.log(runQuiz(createQuiz(questionsList)));
